96152ccc361186546f0585434c72b306b8044cfb
allanamaralalves@gmail.com <allanamaralalves@gmail.com@57b5f7d9-5d4a-7f34-da32-e58669286aa7>
2012-07-04 12:22:31 +0000

    Criação do método updateCliente na classe ClienteDAO.

--- trunk/sistema-para-locadora-de-veiculo/Locadora/src/db/ClienteDAO.py
+++ trunk/sistema-para-locadora-de-veiculo/Locadora/src/db/ClienteDAO.py
@@ -105,6 +105,32 @@ class ClienteDAO(object):
             print "\nCliente Inexistente"   
         finally:
             ConnectionUtil.fecharConexao(cur,conexao)
+    
+    def updateCliente(self, cliente):
+        if(cliente != None):
+            try:                
+                lista = cliente.getAtributos()                             
+                conexao = ConnectionUtil.conectar()
+                with conexao:
+                    cur = conexao.cursor()
+                    if self.verificarExistenciaCliente(cliente.getCpf()) is True:
+                        cur.execute("UPDATE CLIENTES SET NOME =? , ENDERECO = ?, TELEFONE =? , CEP=? , \
+                     BAIRRO=?, CIDADE=? , UF =? , EMAIL =?   WHERE CPF = ?", \
+                                    tuple([lista[0],lista[1],lista[2],lista[3],\
+                                          lista[4],lista[5],lista[6],lista[7], lista[8]]))
+
+                        print "Atualizou no banco"
+                        conexao.commit()
+                    else:
+                        print "CPF nao encontrado!"
+                
+            except lite.Error, e:
+                if conexao:
+                    conexao.rollback()        
+                    print "Error %s:" % e.args[0]
+                    sys.exit(1)    
+            finally:
+                ConnectionUtil.fecharConexao(cur,conexao)
             
 
 
@@ -116,3 +142,6 @@ class ClienteDAO(object):
 #cliente_encontrado1 = dao.procurarCliente("001.111.222-33")
 #cliente_encontrado = dao.procurarCliente("000.111.222-33")
 #cliente_encontrado.toString()
+#cliente_encontrado.setNome("MARCELA DO AMARAL")
+#cliente_encontrado.toString()
+#dao.updateCliente(cliente_encontrado)
