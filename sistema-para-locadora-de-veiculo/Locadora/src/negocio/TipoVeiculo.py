c2caad4e0d0b88f478e4d1f9a400834d1f873cb5
allandoamaral@gmail.com <allandoamaral@gmail.com@57b5f7d9-5d4a-7f34-da32-e58669286aa7>
2012-07-06 02:57:38 +0000

    Criação da classe TipoVeiculoDAO

--- sistema-para-locadora-de-veiculo/Locadora/src/negocio/TipoVeiculo.py
+++ sistema-para-locadora-de-veiculo/Locadora/src/negocio/TipoVeiculo.py
@@ -6,34 +6,43 @@ Created on 05/07/2012
 
 class TipoVeiculo:
 
-    def __init__ (self, taxaBase, precoKm, idTipoVeiculo, descricao, caucao):
-        self.__taxaBase = 0
-        self.__precoKm = precoKm
+    def __init__ (self, idTipoVeiculo, taxaBase, precoKm, descricao, caucao):
         self.__idTipoVeiculo = idTipoVeiculo
+        self.__taxaBase = taxaBase
+        self.__precoKm = precoKm
         self.__descricao = descricao
         self.__caucao = caucao
         
     def getTaxaBase(self):
-        self.__taxaBase
+        return self.__taxaBase
     def setTaxaBase(self, taxaBase):
         self.__taxaBase = taxaBase
     
     def getPrecoKm(self):
-        self.__precoKm
+        return self.__precoKm
     def setPrecoKm(self, precoKm):
         self.__precoKm = precoKm
     
     def getIdTipoVeiculo(self):
-        self.__idTipoVeiculo
+        return self.__idTipoVeiculo
     def setIdTipoVeiculo(self, idTipoVeiculo):
         self.__idTipoVeiculo = idTipoVeiculo
         
     def getDescricao(self):
-        self.__descricao
+        return self.__descricao
     def setDescricao(self, descricao):
         self.__descricao = descricao 
         
     def getCaucao(self):
-        self.__caucao
+        return self.__caucao
     def setCaucao(self, caucao):
         self.__caucao = caucao
+        
+    def getAtributos(self):
+        ID = self.getIdTipoVeiculo()
+        taxaBase = self.getTaxaBase()
+        precoKm = self.getPrecoKm()
+        descricao = self.getDescricao()
+        caucao = self.getCaucao()
+        
+        return [ID, taxaBase, precoKm, descricao, caucao]
