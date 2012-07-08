783f3cf5fa2479d92b4cf3a7d906079a1489616d
allanamaralalves@gmail.com <allanamaralalves@gmail.com@57b5f7d9-5d4a-7f34-da32-e58669286aa7>
2012-07-08 17:56:02 +0000

    Edição Classe VeiculoDAO

--- trunk/sistema-para-locadora-de-veiculo/Locadora/src/db/VeiculoDAO.py
+++ trunk/sistema-para-locadora-de-veiculo/Locadora/src/db/VeiculoDAO.py
@@ -34,7 +34,132 @@ class VeiculoDAO(object):
                     sys.exit(1)   
             finally:
                 ConnectionUtil.fecharConexao(cur, conexao) 
+                
+                
+    def verificarExistenciaVeiculo(self, placa):
+        
+        try:
+            conexao = ConnectionUtil.conectar()
+            with conexao:
+                cur = conexao.cursor()
+                cur.execute("SELECT * FROM VEICULOS WHERE PLACA = ? ", (placa,))
+                print "Verificou no banco"
+                retorno = cur.fetchone()
+                                
+                if retorno is not None:
+                    return True
+                else:
+                    return False
+        except lite.Error, e:
+            if conexao:
+                conexao.rollback()        
+                print "Error %s:" % e.args[0]
+                sys.exit(1)
+        finally:
+            ConnectionUtil.fecharConexao(cur,conexao)
+            
+            
+    def deleteVeiculo(self, placa):
+                
+        try:
+            conexao = ConnectionUtil.conectar()
+            with conexao:
+                cur = conexao.cursor()
+                if self.verificarExistenciaVeiculo(placa) is True:
+                    cur.execute("DELETE FROM VEICULOS WHERE PLACA = ?" , (placa,))
+                    print "Removeu do banco"
+                    conexao.commit()
+                else:
+                    print "Veiculo não encontrado ou já foi removido!"
+            
+        except lite.Error, e:
+            if conexao:
+                conexao.rollback()        
+                print "Error %s:" % e.args[0]
+                sys.exit(1)    
+        finally:
+            ConnectionUtil.fecharConexao(cur,conexao)
+            
+    def procurarVeiculo(self, placa):
+                
+        try:
+            conexao = ConnectionUtil.conectar()
+            with conexao:
+                cur = conexao.cursor()
+                cur.execute("SELECT * FROM VEICULOS WHERE PLACA = ?", (placa,))
+                print "\nBuscou no banco"
+                row = cur.fetchone()
+                print row
+                veiculoEncontrado = Veiculo(row[1],row[2],row[3],row[4],row[6])
+                print veiculoEncontrado.getAtributos()  
+                print veiculoEncontrado.toString()   
+                #veiculoEncontrado.setPlaca(row[1])
+                
+                return veiculoEncontrado
+            
+        except lite.Error, e:
+            if conexao:
+                conexao.rollback()        
+                print "Error %s:" % e.args[0]
+                sys.exit(1) 
+        except:
+            print "\nVeiculo Inexistente"   
+        finally:
+            ConnectionUtil.fecharConexao(cur,conexao)
+    
+    def updateVeiculo(self, veiculo):
+        if(veiculo != None):
+            try:                                                             
+                conexao = ConnectionUtil.conectar()
+                with conexao:
+                    cur = conexao.cursor()
+                    print veiculo.getPlaca()
+                    print self.verificarExistenciaVeiculo(veiculo.getPlaca())
+                    if self.verificarExistenciaVeiculo(veiculo.getPlaca()) is True:
+                        lista = veiculo.getAtributos()
+                        cur.execute("UPDATE VEICULOS SET MARCA=?, COR=?, MODELO=? , \
+                     DISPONIBILIDADE=?, FK_ID_TIPO_VEICULO=?  WHERE PLACA = ?", \
+                                    tuple([lista[1],lista[2],lista[3],lista[4], lista[5]]))
+
+                        print "Atualizou no banco"
+                        conexao.commit()
+                    else:
+                        print "Veiculo de placa nao encontrada!"
+                
+            except lite.Error, e:
+                if conexao:
+                    conexao.rollback()        
+                    print "Error %s:" % e.args[0]
+                    sys.exit(1)    
+            finally:
+                ConnectionUtil.fecharConexao(cur,conexao)
+                
+    def getAllClientes(self):
+        try:    
+            conexao = ConnectionUtil.conectar()
+            with conexao:
+                cur = conexao.cursor()
+                cur.execute("SELECT * FROM VEICULOS ")
+                row = cur.fetchall()      
+                return row
+        except lite.Error, e:
+            if conexao:
+                conexao.rollback()        
+                print "Error %s:" % e.args[0]
+                sys.exit(1)    
+        finally:
+                ConnectionUtil.fecharConexao(cur,conexao)
 
     
         
-        
+#veiculo = Veiculo("WHK-2010","HONDA", "PRATA", "CIVIC" , 30)
+dao = VeiculoDAO()
+#dao.insertVeiculo(veiculo)
+#print dao.verificarExistenciaVeiculo("WHK-2010")
+#dao.deleteVeiculo("WHK-2010")
+#veiculo_encontrado1 = dao.procurarVeiculo("WHK-2010")
+veiculo_encontrado = dao.procurarVeiculo("HZG-1000")
+veiculo_encontrado.toString()
+#veiculo_encontrado.setMarca("CHEVROLET")
+#veiculo_encontrado.toString()
+#dao.updateVeiculo(veiculo_encontrado)
