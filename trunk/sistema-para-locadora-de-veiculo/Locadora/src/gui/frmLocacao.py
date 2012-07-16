560c496cd199ac8e3175ca83cd6ae27ed6d0163a
marcela.domingues@gmail.com <marcela.domingues@gmail.com@57b5f7d9-5d4a-7f34-da32-e58669286aa7>
2012-07-16 00:59:33 +0000

    Criação da ação do botão de pesquisa na classe frmLocacao.

--- trunk/sistema-para-locadora-de-veiculo/Locadora/src/gui/frmLocacao.py
+++ trunk/sistema-para-locadora-de-veiculo/Locadora/src/gui/frmLocacao.py
@@ -2,12 +2,17 @@
 #Boa:Frame:frmLocacao
 
 import wx
+from wx.lib.anchors import LayoutAnchors
+import wx.lib.buttons
 import wx.lib.masked.textctrl
+
+from db.ClienteDAO import *
 
 def create(parent):
     return frmLocacao(parent)
 
-[wxID_FRMLOCACAO, wxID_FRMLOCACAOLISTCTRLBUSCATIPOVEICULO, 
+[wxID_FRMLOCACAO, wxID_FRMLOCACAOBTNCANCELAR, wxID_FRMLOCACAOBTNINCLUIR, 
+ wxID_FRMLOCACAOBTNPESQUISARCPF, wxID_FRMLOCACAOLISTCTRLBUSCATIPOVEICULO, 
  wxID_FRMLOCACAOLSTCTRLBUSCAMODELO, wxID_FRMLOCACAOLSTIPOVEICULO, 
  wxID_FRMLOCACAOPANEL1, wxID_FRMLOCACAORADIOBOXTIPOBUSCA, 
  wxID_FRMLOCACAOSTCLIENTE, wxID_FRMLOCACAOSTCOR, wxID_FRMLOCACAOSTCPF, 
@@ -15,25 +20,40 @@ def create(parent):
  wxID_FRMLOCACAOSTRESULTADOMODELO, wxID_FRMLOCACAOSTRESULTADOTIPO, 
  wxID_FRMLOCACAOSTTIPOVEICULO, wxID_FRMLOCACAOSTVEICULO, 
  wxID_FRMLOCACAOTXTCOR, wxID_FRMLOCACAOTXTCPF, wxID_FRMLOCACAOTXTMODELO, 
-] = [wx.NewId() for _init_ctrls in range(19)]
+] = [wx.NewId() for _init_ctrls in range(22)]
 
 class frmLocacao(wx.Frame):
+    def _init_coll_listCtrlBuscaTipoVeiculo_Columns(self, parent):
+        # generated method, don't edit
+
+        parent.InsertColumn(col=0, format=wx.LIST_FORMAT_LEFT, heading='Modelo',
+              width=300)
+        parent.InsertColumn(col=1, format=wx.LIST_FORMAT_LEFT, heading='Cor',
+              width=150)
+        parent.InsertColumn(col=2, format=wx.LIST_FORMAT_LEFT,
+              heading='Taxa Base - R$', width=100)
+        parent.InsertColumn(col=3, format=wx.LIST_FORMAT_LEFT,
+              heading='Pre\xe7o/KM - R$', width=100)
+        parent.InsertColumn(col=4, format=wx.LIST_FORMAT_LEFT,
+              heading='Cau\xe7\xe3o - R$', width=89)
+
     def _init_ctrls(self, prnt):
         # generated method, don't edit
         wx.Frame.__init__(self, id=wxID_FRMLOCACAO, name='frmLocacao',
-              parent=prnt, pos=wx.Point(346, 0), size=wx.Size(979, 703),
+              parent=prnt, pos=wx.Point(390, 55), size=wx.Size(962, 680),
               style=wx.MINIMIZE_BOX | wx.SYSTEM_MENU | wx.CLOSE_BOX | wx.CAPTION,
               title='Loca\xe7\xe3o de Ve\xedculo')
-        self.SetClientSize(wx.Size(963, 665))
+        self.SetClientSize(wx.Size(946, 642))
         self.SetIcon(wx.Icon(u'../gui/icon/logo.ico',wx.BITMAP_TYPE_ICO))
 
         self.panel1 = wx.Panel(id=wxID_FRMLOCACAOPANEL1, name='panel1',
-              parent=self, pos=wx.Point(0, 0), size=wx.Size(963, 665),
-              style=wx.VSCROLL)
+              parent=self, pos=wx.Point(0, 0), size=wx.Size(946, 642),
+              style=wx.TAB_TRAVERSAL)
 
         self.stCliente = wx.StaticBox(id=wxID_FRMLOCACAOSTCLIENTE,
               label='Cliente', name='stCliente', parent=self.panel1,
               pos=wx.Point(128, 8), size=wx.Size(808, 80), style=0)
+        self.stCliente.Enable(True)
 
         self.stCPF = wx.StaticText(id=wxID_FRMLOCACAOSTCPF, label='CPF :',
               name='stCPF', parent=self.panel1, pos=wx.Point(144, 32),
@@ -41,7 +61,7 @@ class frmLocacao(wx.Frame):
 
         self.txtCPF = wx.lib.masked.textctrl.TextCtrl(id=wxID_FRMLOCACAOTXTCPF,
               name='txtCPF', parent=self.panel1, pos=wx.Point(144, 48),
-              size=wx.Size(176, 24), style=0, value='')
+              size=wx.Size(128, 24), style=0, value='')
         self.txtCPF.SetMask('XXX.XXX.XXX-XX')
         self.txtCPF.SetAutoformat('')
         self.txtCPF.SetDatestyle('MDY')
@@ -50,6 +70,17 @@ class frmLocacao(wx.Frame):
         self.txtCPF.SetExcludeChars('')
         self.txtCPF.SetValidRegex('')
         self.txtCPF.SetMaxLength(14)
+        self.txtCPF.Bind(wx.EVT_TEXT_MAXLEN, self.OnTxtCPFTextMaxlen,
+              id=wxID_FRMLOCACAOTXTCPF)
+        self.txtCPF.Bind(wx.EVT_TEXT_ENTER, self.OnTxtCPFTextMaxlen,
+              id=wxID_FRMLOCACAOTXTCPF)
+
+        self.btnPesquisarCpf = wx.lib.buttons.GenBitmapButton(bitmap=wx.Bitmap(u'../gui/icon/search.png',
+              wx.BITMAP_TYPE_PNG), id=wxID_FRMLOCACAOBTNPESQUISARCPF,
+              name='btnPesquisarCpf', parent=self.panel1, pos=wx.Point(280, 44),
+              size=wx.Size(31, 32), style=0)
+        self.btnPesquisarCpf.Bind(wx.EVT_BUTTON, self.OnBtnPesquisarCpfButton,
+              id=wxID_FRMLOCACAOBTNPESQUISARCPF)
 
         self.stNomeCliente = wx.StaticText(id=wxID_FRMLOCACAOSTNOMECLIENTE,
               label='', name='stNomeCliente', parent=self.panel1,
@@ -61,7 +92,7 @@ class frmLocacao(wx.Frame):
 
         self.stVeiculo = wx.StaticBox(id=wxID_FRMLOCACAOSTVEICULO,
               label='Ve\xedculo', name='stVeiculo', parent=self.panel1,
-              pos=wx.Point(128, 96), size=wx.Size(808, 544), style=0)
+              pos=wx.Point(128, 96), size=wx.Size(808, 536), style=0)
 
         self.radioBoxTipoBusca = wx.RadioBox(choices=['Tipo de veículo',
               'Modelo', 'Cor'], id=wxID_FRMLOCACAORADIOBOXTIPOBUSCA,
@@ -101,24 +132,63 @@ class frmLocacao(wx.Frame):
         self.stResultadoTipo = wx.StaticBox(id=wxID_FRMLOCACAOSTRESULTADOTIPO,
               label='Resultado por tipo de ve\xedculo', name='stResultadoTipo',
               parent=self.panel1, pos=wx.Point(144, 224), size=wx.Size(776,
-              192), style=0)
+              176), style=0)
 
         self.listCtrlBuscaTipoVeiculo = wx.ListCtrl(id=wxID_FRMLOCACAOLISTCTRLBUSCATIPOVEICULO,
               name='listCtrlBuscaTipoVeiculo', parent=self.panel1,
-              pos=wx.Point(160, 248), size=wx.Size(744, 152),
+              pos=wx.Point(160, 248), size=wx.Size(744, 136),
               style=wx.LC_REPORT)
+        self._init_coll_listCtrlBuscaTipoVeiculo_Columns(self.listCtrlBuscaTipoVeiculo)
 
         self.stResultadoModelo = wx.StaticBox(id=wxID_FRMLOCACAOSTRESULTADOMODELO,
               label='Resultado por modelo do ve\xedculo',
               name='stResultadoModelo', parent=self.panel1, pos=wx.Point(144,
-              428), size=wx.Size(776, 196), style=0)
+              412), size=wx.Size(776, 204), style=0)
 
         self.lstCtrlBuscaModelo = wx.ListCtrl(id=wxID_FRMLOCACAOLSTCTRLBUSCAMODELO,
               name='lstCtrlBuscaModelo', parent=self.panel1, pos=wx.Point(160,
-              448), size=wx.Size(744, 160), style=wx.LC_ICON)
+              440), size=wx.Size(744, 160), style=wx.LC_ICON)
+
+        self.btnIncluir = wx.lib.buttons.GenButton(id=wxID_FRMLOCACAOBTNINCLUIR,
+              label='Incluir', name='btnIncluir', parent=self.panel1,
+              pos=wx.Point(24, 24), size=wx.Size(76, 25), style=0)
+        self.btnIncluir.SetConstraints(LayoutAnchors(self.btnIncluir, True,
+              True, False, False))
+
+        self.btnCancelar = wx.lib.buttons.GenButton(id=wxID_FRMLOCACAOBTNCANCELAR,
+              label='Cancelar', name='btnCancelar', parent=self.panel1,
+              pos=wx.Point(24, 64), size=wx.Size(76, 25), style=0)
+        self.btnCancelar.Bind(wx.EVT_BUTTON, self.OnBtnCancelarButton,
+              id=wxID_FRMLOCACAOBTNCANCELAR)
 
     def __init__(self, parent):
         self._init_ctrls(parent)
+
+    def OnTxtCPFTextMaxlen(self, event):
+        event.Skip()
+
+    def OnTxtCPFTextEnter(self, event):
+        event.Skip()
+
+    def OnBtnPesquisarCpfButton(self, event):  
+        cpf = self.txtCPF.GetValue() 
+        #print cpf
+        dao = ClienteDAO()
+        #print dao.verificarExistenciaCliente(cpf)
+        if(dao.verificarExistenciaCliente(cpf) is True):
+            cliente = dao.procurarCliente(cpf)
+            self.stNomeCliente.SetLabel(cliente.getNome())   
+        else:
+            caixaDeDialogo = wx.MessageDialog(self,'Cliente inexistente.', 'ERRO!', wx.OK | wx.ICON_INFORMATION)
+            caixaDeDialogo.ShowModal()
+            caixaDeDialogo.Destroy()
+            
+            self.txtCPF.Clear()
+
+    def OnBtnCancelarButton(self, event):
+        event.Skip()
+
+    
         
 if __name__ == '__main__':
     app = wx.PySimpleApp()
