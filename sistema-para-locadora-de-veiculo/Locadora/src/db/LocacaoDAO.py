e2010728674f7a1ed434978b499da96137360085
allanamaralalves@gmail.com <allanamaralalves@gmail.com@57b5f7d9-5d4a-7f34-da32-e58669286aa7>
2012-07-16 00:27:43 +0000

    Criação dos métodos updateLocacao e getAllLocacoes na classe LocacaoDAO.

--- sistema-para-locadora-de-veiculo/Locadora/src/db/LocacaoDAO.py
+++ sistema-para-locadora-de-veiculo/Locadora/src/db/LocacaoDAO.py
@@ -105,16 +105,62 @@ class LocacaoDAO(object):
             print "\nLocação Inexistente"   
         finally:
             ConnectionUtil.fecharConexao(cur,conexao)
+            
+    def updateLocacao(self, locacao):
+        if(locacao != None):
+            try:                                                             
+                conexao = ConnectionUtil.conectar()
+                with conexao:
+                    cur = conexao.cursor()
+                    #idLocacao = locacao.getIdLocacao()
+                    if self.verificarExistenciaLocacao(locacao.getIdLocacao()) is True:
+                        lista = locacao.getAtributos()
+                        cur.execute("UPDATE LOCACAO SET DATA_LOCACAO = ?, QUILOMETRAGEM_SAIDA = ?, \
+                                    VALOR_CONTA_PARCIAL = ? ,  FK_CPF_CLIENTE = ?, FK_ID_VEICULO = ? \
+                                    WHERE ID_LOCACAO = ?", \
+                                    tuple([lista[0],lista[1],lista[2],lista[3],\
+                                          lista[4], locacao.getIdLocacao()]))
+
+                        print "Atualizou no banco"
+                        conexao.commit()
+                    else:
+                        print "Locação não encontrada!"
+                
+            except lite.Error, e:
+                if conexao:
+                    conexao.rollback()        
+                    print "Error %s:" % e.args[0]
+                    sys.exit(1)    
+            finally:
+                ConnectionUtil.fecharConexao(cur,conexao)
+    
+    def getAllLocacoes(self):
+        try:    
+            conexao = ConnectionUtil.conectar()
+            with conexao:
+                cur = conexao.cursor()
+                cur.execute("SELECT * FROM LOCACAO ")
+                row = cur.fetchall()      
+                return row
+        except lite.Error, e:
+            if conexao:
+                conexao.rollback()        
+                print "Error %s:" % e.args[0]
+                sys.exit(1)    
+        finally:
+                ConnectionUtil.fecharConexao(cur,conexao)
                 
 
 #hoje = datetime.today()
 #print hoje
 #locacao = Locacao(hoje,1200,833,'000.111.222-33',28)
-#dao = LocacaoDAO()
+dao = LocacaoDAO()
 #dao.insertLocacao(locacao)
 #print dao.verificarExistenciaLocacao(5)
 #print dao.verificarExistenciaLocacao(95)
 #dao.deleteLocacao(4)
 #locacaoEncontrada = dao.procurarLocacaoById(5)
-#locacaoEncontrada.toString()    
-        
+#locacaoEncontrada.toString() 
+#locacaoEncontrada.setIdCliente("111.222.333-44")
+#dao.updateLocacao(locacaoEncontrada)   
+print dao.getAllLocacoes()        
