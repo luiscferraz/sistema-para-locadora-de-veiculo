ca87674832595f30bc0dd56d85cbeaa553ccc4ba
allanamaralalves@gmail.com <allanamaralalves@gmail.com@57b5f7d9-5d4a-7f34-da32-e58669286aa7>
2012-08-19 05:47:32 +0000

    Pasta 'Modulos para pdf' reparada.

---
+++ trunk/sistema-para-locadora-de-veiculo/Locadora/src/negocio/Impressao.py
@@ -0,0 +1,23 @@
+#-*- coding:utf-8 -*-
+'''
+Created on 19/07/2012
+
+@author: 
+'''
+from reportlab.pdfgen.canvas import Canvas
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.units import cm, mm, inch, pica
+import os
+
+
+class Impressao(object):
+        
+    namefile = '../../../Comprovantes/teste.pdf'
+    pdf = Canvas(namefile, pagesize = letter) #Nome do arquivo e Tipo do papel
+    pdf.setFont("Courier",12) #Seta a fonte para Courier tamanho 12
+    pdf.setStrokeColorRGB(1, 0, 0)
+    pdf.drawString(100, 750, "MEU PRIMEIRO PDF") #Escreve o texto na posição (0,0) = (x,y)
+    pdf.showPage()
+    pdf.save()
+    
+    #os.system();
